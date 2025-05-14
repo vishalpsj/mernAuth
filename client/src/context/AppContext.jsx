dfe3d24d5ea75472e3f048a7d0ae5cde04bcce4c
Vishal P <73173958+vishalpsj@users.noreply.github.com>
2025-05-14 22:28:51 +0530

    Update AppContext.jsx
    updated backendurl

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,8 +7,8 @@ export const AppContext = createContext()
 export const AppContextProvider = (props) => {
 
     axios.defaults.withCredentials = true;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://mernauth-backend-uvxl.onrender.com';
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [userData, setUserData] = useState(false)
 
